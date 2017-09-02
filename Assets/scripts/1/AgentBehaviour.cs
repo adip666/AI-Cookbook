04f9f4215f45cfe41b80efb4ef9274466a1ce1c4
adip666 <adrian.przydatek@hotmail.com>
2017-09-02 17:36:15 +0200

    path

--- Assets/scripts/1/AgentBehaviour.cs
+++ Assets/scripts/1/AgentBehaviour.cs
@@ -41,4 +41,12 @@ public class AgentBehaviour : MonoBehaviour {
         return rotation;
 
     }
+
+    public Vector3 OriToVec(float orientation)
+    {
+        Vector3 vector = Vector3.zero;
+        vector.x = Mathf.Sin(orientation * Mathf.Deg2Rad) * 1f;
+        vector.z = Mathf.Cos(orientation * Mathf.Deg2Rad) * 1f;
+        return vector.normalized;
+    }
 }
